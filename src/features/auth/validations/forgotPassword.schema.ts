702d96af36b43ee5e2b4792792b895ff159337cb
Javad Jazz <javadjazz899@gmail.com>
2026-07-05 00:51:08 +0530

    done

--- src/features/auth/validations/forgotPassword.schema.ts
+++ src/features/auth/validations/forgotPassword.schema.ts
@@ -1,5 +1,6 @@
 import * as yup from 'yup';
+import { PHONE_NUMBER_REGEX, PHONE_NUMBER_MESSAGE } from '../../../shared/constants/regex';
 
 export const forgotPasswordValidationSchema = yup.object({
-  phone: yup.string().required('Phone number is required'),
+  phone: yup.string().required('Phone number is required').matches(PHONE_NUMBER_REGEX, PHONE_NUMBER_MESSAGE),
 });
