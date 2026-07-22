c2dd356433dcd02ac8964bfb95d1f15b9a503043
Javad Jazz <javadjazz899@gmail.com>
2026-07-22 13:27:15 +0530

    done

--- src/features/lead_settings/lead-types/types/index.ts
+++
@@ -1,1 +0,0 @@
-export interface LeadTypeItem { id: number; addedBy: string; type: string; }
--- src/shared/components/table/TableRow.tsx
+++
@@ -1,8 +0,0 @@
-import type { TableRowProps } from './types';
-
-const TableRow = ({ selected, children, className }: TableRowProps) => {
-  const classes = [className, selected ? 'selected' : ''].filter(Boolean).join(' ');
-  return <tr className={classes || undefined}>{children}</tr>;
-};
-
-export default TableRow;
