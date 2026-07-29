a5139267af78480f6fb754d904b75dec7020f96a
NITHIN_360 <nithinbalakrishnan569@gmail.com>
2026-07-29 16:33:52 +0530

    role-based-authentication.

--- src/features/auth/services/AuthService.ts
+++ src/features/auth/services/AuthService.ts
@@ -1,10 +1,24 @@
-import axiosInstance from '../../../api/axiosInstance';
-import type { LoginRequest, LoginResponse, ForgotPasswordRequest, ForgotPasswordResponse, ResetPasswordRequest, ResetPasswordResponse } from '../types/auth.types';
-import { AUTH_API_ENDPOINTS } from '../constants/authApiEndpoints';
+import axiosInstance from "../../../api/axiosInstance";
+import type {
+  LoginRequest,
+  LoginResponse,
+  ForgotPasswordRequest,
+  ForgotPasswordResponse,
+  ResetPasswordRequest,
+  ResetPasswordResponse,
+} from "../types/auth.types";
+import { AUTH_API_ENDPOINTS } from "../constants/authApiEndpoints";
 
 class AuthService {
   async login(data: LoginRequest): Promise<LoginResponse> {
-    const response = await axiosInstance.post<LoginResponse>(AUTH_API_ENDPOINTS.LOGIN, data);
+    const response = await axiosInstance.post<LoginResponse>(
+      AUTH_API_ENDPOINTS.LOGIN,
+      {
+        ...data,
+        is_limited: true,
+      },
+    );
+
     return {
       status: response.data.status,
       message: response.data.message,
@@ -12,16 +26,26 @@ class AuthService {
     };
   }
 
-  async forgotPassword(data: ForgotPasswordRequest): Promise<ForgotPasswordResponse> {
-    const response = await axiosInstance.post<ForgotPasswordResponse>(AUTH_API_ENDPOINTS.FORGOT_PASSWORD, data);
+  async forgotPassword(
+    data: ForgotPasswordRequest,
+  ): Promise<ForgotPasswordResponse> {
+    const response = await axiosInstance.post<ForgotPasswordResponse>(
+      AUTH_API_ENDPOINTS.FORGOT_PASSWORD,
+      data,
+    );
     return {
       status: response.data.status,
       message: response.data.message,
     };
   }
 
-  async resetPassword(data: ResetPasswordRequest): Promise<ResetPasswordResponse> {
-    const response = await axiosInstance.post<ResetPasswordResponse>(AUTH_API_ENDPOINTS.RESET_PASSWORD, data);
+  async resetPassword(
+    data: ResetPasswordRequest,
+  ): Promise<ResetPasswordResponse> {
+    const response = await axiosInstance.post<ResetPasswordResponse>(
+      AUTH_API_ENDPOINTS.RESET_PASSWORD,
+      data,
+    );
     return {
       status: response.data.status,
       message: response.data.message,
