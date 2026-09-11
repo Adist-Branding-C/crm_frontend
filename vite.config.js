401b9476bbd1bde8adb709e79dde12ba22ea7045
Muhammed Javad <75117754+muhammedjavadksd@users.noreply.github.com>
2026-09-11 14:35:20 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -11,8 +11,7 @@ export default defineConfig({
       'f0cb-2401-4900-8fdc-2d68-f1eb-d12a-f752-7f17.ngrok-free.app',
       'a075-2401-4900-8fdc-2d68-8534-50f7-4058-bf48.ngrok-free.app',
       'glory-facial-our-shadow.trycloudflare.com',
-      'leadistcrm.com',
-      'www.leadistcrm.com',
+      'app.leadistcrm.com',
     ],
   },
 })
